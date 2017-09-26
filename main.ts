876d4bc3d9009302fee5a2b8cf7c5219ec188515
Daniel Hutchinson <danielhutchinson5@gmail.com>
2017-09-26 09:45:03 +0100

    an easier observer pattern

--- main.ts
+++ main.ts
@@ -3,18 +3,11 @@ import { Observable, Observer } from 'rxjs'
 let numbers = [1,5,10]
 let source = Observable.from(numbers)
 
-class MyObserver implements Observer<number> {
-    next(value) {
-        console.log(`value: ${value}`)
-    }
-
-    error(error) {
-        console.log(`error ${error}`)
-    }
-
-    complete() {
-        console.log('complete')
-    }
-}
-
-source.subscribe(new MyObserver())
+source.subscribe(
+    // next
+    nextValue => console.log(`nextValue: ${nextValue}`),
+    // error
+    error => console.log(`error: ${error}`),
+    // complete
+    () => console.log('complete')
+)
